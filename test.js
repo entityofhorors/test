4d955427a56c50377e4d8fcfaab016a8f96eb7e2
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-20 22:19:06 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,36 +2,20 @@
 // URANIUM ENRICHMENT MOD BY K.T
 // *****************************
 
-currentColor = "#80ffff";
-function deletecoconut() {
-    throw "The game crashed, because you deleted the coconut."
-}
-let rPOWDER = behaviors.POWDER
-console.log("Welcome to the console.");
-console.log(rPOWDER);
+runAfterLoad(function() {
+    
+ // ---------------- Vanadium Ore ----------------
+    elements.vanadium_ore = {
+        color: "#38ce31",
+        behavior: behaviors.POWDER,
+        category: "powders",
+        state: "solid",
+        density: 3500,
+        tempHigh: 1500,
+        stateHigh: "steam",
+                    }
+                }
+            }
+        }
+    });
 
-// ---------------- Vanadium Ore ----------------
-elements.vanadium_ore = {
-    name: "Vandium",
-    color: ["#545454", "#3b3b3b", "#8c8c8c"],
-    behavior: behaviors.POWDER,
-    category: "powders",
-    state: "solid",
-    density: 6.1,
-    temp: 20,
-    tempHigh: 690,
-    stateHigh: "molten_vanadium_ore",
-     }
-};
-// ---------------- Molten Vanadium Ore ----------------
-elements.molten_vanadium_ore = {
-    color: ["#770000", "#570000", "#990000"],
-    behavior: behaviors.LIQUID,
-    category: "states",
-    state: "liquid",
-    density: 6.1,
-    temp: 20,
-    tempLow: 690,
-    stateLow: "vanadium_ore",
-    }
-};
