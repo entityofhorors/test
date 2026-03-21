503b4abba9319f41b9ec349daeef834c0146a13c
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-20 23:59:45 -0400

    Update test.js

--- test.js
+++ test.js
@@ -46,13 +46,11 @@
     };
  // ---------------- Vanadium Slurry ----------------
     elements.vanadium_slurry = {
-        color: "#38ce31",
-        behavior: behaviors.POWDER,
-        category: "powders",
-        state: "solid",
-        density: 3500,
-        tempHigh: 1500,
-        stateHigh: "molten_vanadium_ore",
+        color: "#7a7a7a",
+        behavior: behaviors.LIQUID,
+        category: "liquids",
+        state: "liquid",
+        density: 2500,
                   tick: function(pixel) {
             let coords = [
                 {x: pixel.x+1, y: pixel.y},
@@ -67,7 +65,79 @@
                 let n = pixelMap[c.x][c.y];
                 if (!n) continue;
 
-                if (n.element === "salt") {
+                if (n.element === "sodium") {
+                    if (Math.random() < 0.1) {
+                        changePixel(pixel, "sodium_vanadate");
+                        deletePixel(c.x, c.y);
+                    }
+                }
+            }
+        }
+     };
+ // ---------------- Sodium Vanadate ----------------
+    elements.sodium_vanadate = {
+        color: "#ff8000",
+        behavior: behaviors.POWDER,
+        category: "powders",
+        state: "solid",
+        density: 1500,
+                  tick: function(pixel) {
+            let coords = [
+                {x: pixel.x+1, y: pixel.y},
+                {x: pixel.x-1, y: pixel.y},
+                {x: pixel.x, y: pixel.y+1},
+                {x: pixel.x, y: pixel.y-1},
+            ];
+
+            for (let c of coords) {
+                if (!pixelMap[c.x] || !pixelMap[c.x][c.y]) continue;
+
+                let n = pixelMap[c.x][c.y];
+                if (!n) continue;
+
+                if (n.element === "water") {
+                    if (Math.random() < 0.1) {
+                        changePixel(pixel, "vanadium");
+                        deletePixel(c.x, c.y);
+                    }
+                }
+            }
+        }
+     };
+ // ---------------- Vanadium ----------------
+    elements.vanadium = {
+        color: "#a6a6a6",
+        behavior: behaviors.SOLID,
+        category: "solids",
+        state: "solid",
+        density: 6110,
+        tempHigh: 1900,
+        stateHigh: "molten_vanadium",
+     };
+ // ---------------- Molten Vanadium ----------------
+    elements.molten_vanadium = {
+        color: ["#ff8000", "#ff4d00", "#ff1e00"],
+        behavior: behaviors.LIQUID,
+        category: "states",
+        state: "liquid",
+        density: 6110,
+        tempLow: 1900,
+        stateLow: "vanadium",
+                  tick: function(pixel) {
+            let coords = [
+                {x: pixel.x+1, y: pixel.y},
+                {x: pixel.x-1, y: pixel.y},
+                {x: pixel.x, y: pixel.y+1},
+                {x: pixel.x, y: pixel.y-1},
+            ];
+
+            for (let c of coords) {
+                if (!pixelMap[c.x] || !pixelMap[c.x][c.y]) continue;
+
+                let n = pixelMap[c.x][c.y];
+                if (!n) continue;
+
+                if (n.element === "oxygen") {
                     if (Math.random() < 0.1) {
                         changePixel(pixel, "steam");
                         deletePixel(c.x, c.y);
