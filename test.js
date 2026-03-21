8e75748555d4317b28697de6f5c8cb78aa19f8d3
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-21 06:41:05 -0400

    Update test.js

--- test.js
+++ test.js
@@ -176,3 +176,19 @@
             }
         }
      };
+ // ---------------- Sulfur Trioxide ----------------
+    elements.sulfur_trioxide = {
+        color: "##fff2c2",
+        behavior: behaviors.GAS,
+        category: "gases",
+        state: "gas",
+        density: 1920,
+
+     };
+ // ---------------- Sulfur Trioxide Reaction ----------------
+    elements.oxygen.reactions.sulfur_gas = {
+        elem1: null, 
+        elem2: "sulfur_trioxide", 
+          
+      };
+
