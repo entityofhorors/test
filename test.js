90e5c81520317da5aa7201c6cb12cb22b029dedd
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-20 23:10:27 -0400

    Update test.js

--- test.js
+++ test.js
@@ -10,6 +10,37 @@
         state: "solid",
         density: 3500,
         tempHigh: 1500,
-        stateHigh: "steam",
+        stateHigh: "molten_vanadium_ore",
         };
-  
+ // ---------------- Molten Vanadium Ore ----------------
+    elements.molten_vanadium_ore = {
+        color: "#38ce31",
+        behavior: behaviors.LIQUID,
+        category: "states",
+        state: "liquid",
+        density: 3500,
+        tempLow: 1500,
+        stateLow: "vanadium_ore",
+             tick: function(pixel) {
+            let coords = [
+                {x: pixel.x+1, y: pixel.y},
+                {x: pixel.x-1, y: pixel.y},
+                {x: pixel.x, y: pixel.y+1},
+                {x: pixel.x, y: pixel.y-1},
+            ];
+
+            for (let c of coords) {
+                if (!pixelMap[c.x] || !pixelMap[c.x][c.y]) continue;
+
+                let n = pixelMap[c.x][c.y];
+                if (!n) continue;
+
+                if (n.element === "salt") {
+                    if (Math.random() < 0.1) {
+                        changePixel(pixel, "steam");
+                        deletePixel(c.x, c.y);
+                    }
+                }
+            }
+        }
+    };
