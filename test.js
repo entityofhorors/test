6ff4cf21cf487b52f49dd3c453004f6999af83ab
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-20 23:35:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,48 @@
 
  // ---------------- Vanadium Ore ----------------
     elements.vanadium_ore = {
+        color: ["#403933", "#292725", "#858585",],
+        behavior: behaviors.POWDER,
+        category: "powders",
+        state: "solid",
+        density: 3500,
+        tempHigh: 1500,
+        stateHigh: "molten_vanadium_ore",
+                  tick: function(pixel) {
+            let coords = [
+                {x: pixel.x+1, y: pixel.y},
+                {x: pixel.x-1, y: pixel.y},
+                {x: pixel.x, y: pixel.y+1},
+                {x: pixel.x, y: pixel.y-1},
+            ];
+
+            for (let c of coords) {
+                if (!pixelMap[c.x] || !pixelMap[c.x][c.y]) continue;
+
+                let n = pixelMap[c.x][c.y];
+                if (!n) continue;
+
+                if (n.element === "water") {
+                    if (Math.random() < 0.1) {
+                        changePixel(pixel, "vanadium_slurry");
+                        deletePixel(c.x, c.y);
+                    }
+                }
+            }
+        }
+     };
+ // ---------------- Molten Vanadium Ore ----------------
+    elements.molten_vanadium_ore = {
+        color: ["#deae00", "#960000", "#d43f00",],
+        behavior: behaviors.LIQUID,
+        category: "states",
+        state: "liquid",
+        density: 3500,
+        tempLow: 1500,
+        stateLow: "vanadium_ore",
+    };
+ // ---------------- Vanadium Slurry ----------------
+    elements.vanadium_slurry = {
         color: "#38ce31",
         behavior: behaviors.POWDER,
         category: "powders",
@@ -11,17 +53,7 @@
         density: 3500,
         tempHigh: 1500,
         stateHigh: "molten_vanadium_ore",
-        };
- // ---------------- Molten Vanadium Ore ----------------
-    elements.molten_vanadium_ore = {
-        color: "#38ce31",
-        behavior: behaviors.LIQUID,
-        category: "states",
-        state: "liquid",
-        density: 3500,
-        tempLow: 1500,
-        stateLow: "vanadium_ore",
-             tick: function(pixel) {
+                  tick: function(pixel) {
             let coords = [
                 {x: pixel.x+1, y: pixel.y},
                 {x: pixel.x-1, y: pixel.y},
@@ -43,4 +75,4 @@
                 }
             }
         }
-    };
+     };
