800104f824ee851bbd159b75077ed4163f75bd1a
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-20 20:24:17 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,16 +4,17 @@
 
 // ---------------- Vanadium Ore ----------------
 elements.vanadium_ore = {
+    name: "Vandium",
     color: ["#545454", "#3b3b3b", "#8c8c8c"],
     behavior: behaviors.POWDER,
     category: "powders",
     state: "solid",
+    temp: 20,
     tempHigh: 690,
-    stateHigh: molten_vanadium_ore,
+    stateHigh: "molten_vanadium_ore",
     density: 6.1
-
-                };
-
+     }
+};
 // ---------------- Molten Vanadium Ore ----------------
 elements.molten_vanadium_ore = {
     color: ["#770000", "#570000", "#990000"],
@@ -21,8 +22,7 @@ elements.molten_vanadium_ore = {
     category: "states",
     state: "solid",
     tempLow: 690,
-    stateLow: vanadium_ore,
+    stateLow: "vanadium_ore",
     density: 6.1
-
-                };
-
+    }
+};
