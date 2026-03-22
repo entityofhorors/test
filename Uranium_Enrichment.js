a503c392ed192a379cd58fba22089b96bbe07eb4
entityofhorors <161178096+entityofhorors@users.noreply.github.com>
2026-03-22 11:22:16 -0400

    Update Uranium_Enrichment.js

--- Uranium_Enrichment.js
+++ Uranium_Enrichment.js
@@ -533,7 +533,7 @@
         behavior: [ 
          "CR:radiation%.05|CR:radiation%.05|CR:radiation%.05",
          "CR:radiation%.05|DL%2|CR:radiation%.05",
-         "CR:radiation%.05|M1 AND BO THEN DL|CR:radiation%.05",
+         "CR:radiation%.05|M1 AND BO|CR:radiation%.05",
     ],
         category: "energy",
         state: "gas",
